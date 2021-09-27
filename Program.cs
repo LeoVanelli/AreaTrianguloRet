c59d899e9bdc56ecd8dcff948a9df9d4e0150144
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:38:16 -0300

    Adiciona atividade AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Calcule a área de um triângulo retângulo, dados base (b) e altura (h). A área é dada pela metade do produto da base pela altura.
+
+            Console.Write("Digite o valor da base: ");
+            double b = double.Parse(Console.ReadLine()); 
+
+            Console.Write("Digite o valor da altura: ");
+            double h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Base: {b}\nAltura: {h}\nÁrea do triângulo retângulo: {(b*h)/2}");
+        }
+    }
+}
